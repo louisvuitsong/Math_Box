5cc1d7ae25a4e477093e60c24465c68ab085050e
changwook <scw9327@naver.com>
2022-12-14 16:28:02 +0900

    Add getSum_ver2 function

--- calculator.py
+++ calculator.py
@@ -38,5 +38,8 @@ def get_Percent(a, b):
     return (a/b) * 100
 
 
-def getSum_ver1(n):
-    return n(n+1)/2
+def getSum_ver2(n):
+    sum = 0
+    for i in range(1, n+1):
+        sum = sum + i
+    return sum
